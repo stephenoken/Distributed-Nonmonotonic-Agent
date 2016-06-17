c5453d1d3397e0c354621f280d66a6b497abb775
Stephen O'Kennedy <stephenoken@gmail.com>
2016-06-17 12:32:04 +0100

    "Added defualt options to python script"
    ""

--- run_deimos.py
+++ run_deimos.py
@@ -6,7 +6,8 @@ import datetime as dt
 import shutil
 
 parser = argparse.ArgumentParser(description='Run Deimos Program and out contents to file in theories directory.')
-parser.add_argument( '-t', '--theory', type=str, help="Theory file path")
+parser.add_argument( '-t', '--theory', type=str, help="Theory file path", default="./theories/01huskies.t")
+parser.add_argument('-p', '--proof', type=str, help="Proof to run against theory", default="+d b\nq")
 args = parser.parse_args()
 
 dest_dir_name = "./theories/" + args.theory[args.theory.rfind("/")+1:-2]
@@ -16,6 +17,7 @@ if os.path.isdir(dest_dir_name):
 
 target_dir_path = args.theory[:-2] + "/"
 target_file_dest  = target_dir_path +  str(dt.datetime.now()).replace(" ","") + ".txt"
-
+proof = args.proof if args.proof.endswith("q") else args.proof + "\nq"
+echoProof = "echo \"" + proof + "\"|"
 os.mkdir(target_dir_path)
-os.system("time ./bin/i386-Darwin/DProver " + args.theory + " > " + target_file_dest)
+os.system(echoProof + "time ./bin/i386-Darwin/DProver " + args.theory + " > " + target_file_dest)
