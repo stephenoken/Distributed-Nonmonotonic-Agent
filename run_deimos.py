9f77daeff3d9b99077e632480360cd55f692501c
Stephen O'Kennedy <stephenoken@gmail.com>
2016-06-21 12:33:59 +0100

    "updated the python script"
    "accepts regular expression arguments to see the sequence of trace statements"

--- run_deimos.py
+++ run_deimos.py
@@ -8,6 +8,8 @@ import shutil
 parser = argparse.ArgumentParser(description='Run Deimos Program and out contents to file in theories directory.')
 parser.add_argument( '-t', '--theory', type=str, help="Theory file path", default="./theories/01huskies.t")
 parser.add_argument('-p', '--proof', type=str, help="Proof to run against theory", default="+d b\nq")
+parser.add_argument('-rg', '--regexp', type=str, help="Enter regular expression", default="\"In DInfernce\"")
+parser.add_argument('-n', '--num_of_files', type=int, help="Number of files to generate", default=1)
 args = parser.parse_args()
 
 dest_dir_name = "./theories/" + args.theory[args.theory.rfind("/")+1:-2]
@@ -23,7 +25,10 @@ def run_deimos():
     proof = args.proof if args.proof.endswith("q") else args.proof + "\nq"
     echoProof = "echo \"" + proof + "\"|"
     os.system(echoProof + " ./bin/i386-Darwin/DProver " + args.theory + ">& " + target_file_dest)
+    os.system("grep -n " + args.regexp + " " + target_file_dest)
+    print os.linesep
 
 
 
-run_deimos()
+for n in range(0,args.num_of_files):
+    run_deimos()
